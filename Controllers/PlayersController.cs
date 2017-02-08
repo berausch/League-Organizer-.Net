7354bf8ae4a7bf29e97f9d9bec11e107db166488
Brittany Rausch and Ryan Mathisen <brittany+ryan@>
2017-02-07 16:43:28 -0800

    Finished CRUD Functionality

--- Controllers/PlayersController.cs
+++ Controllers/PlayersController.cs
@@ -34,5 +34,47 @@ namespace LeagueOrganizer.Controllers
             return RedirectToAction("Index");
         }
 
+        public IActionResult Edit(int id)
+        {
+            var selectedPlayer = db.Players.FirstOrDefault(players => players.PlayerId == id);
+            ViewBag.TeamId = new SelectList(db.Teams, "TeamId", "Name");
+            return View(selectedPlayer);
+        }
+        [HttpPost]
+        public IActionResult Edit(Player player)
+        {
+            db.Entry(player).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Details(int id)
+        {
+            var selectPlayer = db.Players
+                .Include(players => players.Team)
+                .FirstOrDefault(players => players.PlayerId == id);
+
+            var playerDivId = db.Teams.FirstOrDefault(teams => teams.TeamId == selectPlayer.TeamId);
+
+            var playerDiv = db.Divisions.FirstOrDefault(divisions => divisions.DivId == playerDivId.DivId);
+
+            ViewBag.playerDiv = playerDiv.Name;
+
+            return View(selectPlayer);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var thisPlayer = db.Players.FirstOrDefault(players => players.PlayerId == id);
+            return View(thisPlayer);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisPlayer = db.Players.FirstOrDefault(players => players.PlayerId == id);
+            db.Players.Remove(thisPlayer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
     }
 }
