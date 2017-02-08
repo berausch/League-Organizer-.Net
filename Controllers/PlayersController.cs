a9379ba54b10966fb2462e06b32b4a7fc7914c66
Brittany Rausch and Ryan Mathisen <brittany+ryan@>
2017-02-07 16:15:19 -0800

    Created more CRUD functionality for Teams and Players

--- Controllers/PlayersController.cs
+++ Controllers/PlayersController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using LeagueOrganizer.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +13,26 @@ namespace LeagueOrganizer.Controllers
 {
     public class PlayersController : Controller
     {
+        private LeagueContext db = new LeagueContext();
         // GET: /<controller>/
         public IActionResult Index()
         {
+            return View(db.Players.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.TeamId = new SelectList(db.Teams, "TeamId", "Name");
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Create(Player newPlayer)
+        {
+            db.Players.Add(newPlayer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
--- Controllers/TeamsController.cs
+++ Controllers/TeamsController.cs
@@ -25,11 +25,62 @@ namespace LeagueOrganizer.Controllers
                 .Include(teams => teams.Players)
                 .Include(teams => teams.Division)
                 .FirstOrDefault(teams => teams.TeamId == id);
+            if(selectTeam.CaptId == 0)
+            {
+                ViewBag.Captain = "None";
 
-            var captain = db.Players.FirstOrDefault(players => players.PlayerId == selectTeam.CaptId);
+            } else {
 
-            ViewBag.Captain = captain.FName + " " + captain.LName;
+                var captain = db.Players.FirstOrDefault(players => players.PlayerId == selectTeam.CaptId);
+
+                ViewBag.Captain = captain.FName + " " + captain.LName;
+            }
+
             return View(selectTeam);
         }
+
+        public IActionResult Edit(int id)
+        {
+            var selectedTeam = db.Teams.FirstOrDefault(teams => teams.TeamId == id);
+            ViewBag.DivId = new SelectList(db.Divisions, "DivId", "Name");
+            ViewBag.CaptId = new SelectList(db.Players.Where(p => p.TeamId == id), "PlayerId", "LName");
+            return View(selectedTeam);
+        }
+        [HttpPost]
+        public IActionResult Edit(Team team)
+        {
+            db.Entry(team).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var thisTeam = db.Teams.FirstOrDefault(teams => teams.TeamId == id);
+            return View(thisTeam);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisTeam = db.Teams.FirstOrDefault(teams => teams.TeamId == id);
+            db.Teams.Remove(thisTeam);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.DivId = new SelectList(db.Divisions, "DivId", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Team newTeam)
+        {
+            db.Teams.Add(newTeam);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
